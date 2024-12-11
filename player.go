afb9be50279c4c91cb45b4642741ddb2cc94b9b3
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-12-12 03:18:41 +0800

    style(mprisPlayer): remove extra lines

--- player.go
+++ player.go
@@ -38,25 +38,21 @@ func (p *player) props() map[string]*prop.Prop {
 	}
 
 	p.propsSpec = map[string]*prop.Prop{
-		"PlaybackStatus": {
-			Value:    playbackStopped,
-			Writable: false,
-			Emit:     prop.EmitTrue,
-		},
-		"LoopStatus":    {Value: loopNone, Writable: true, Emit: prop.EmitTrue},
-		"Rate":          {Value: float64(1), Writable: true, Emit: prop.EmitTrue},
-		"Shuffle":       {Value: false, Writable: true, Emit: prop.EmitTrue},
-		"Metadata":      {Value: playerMetadata{}, Writable: false, Emit: prop.EmitTrue},
-		"Volume":        {Value: float64(0), Writable: true, Emit: prop.EmitTrue},
-		"Position":      {Value: int64(0), Writable: false, Emit: prop.EmitFalse},
-		"MinimumRate":   {Value: playerMinimumRate, Writable: false, Emit: prop.EmitTrue},
-		"MaximumRate":   {Value: playerMaximumRate, Writable: false, Emit: prop.EmitTrue},
-		"CanGoNext":     {Value: true, Writable: false, Emit: prop.EmitTrue},
-		"CanGoPrevious": {Value: true, Writable: false, Emit: prop.EmitTrue},
-		"CanPlay":       {Value: true, Writable: false, Emit: prop.EmitTrue},
-		"CanPause":      {Value: true, Writable: false, Emit: prop.EmitTrue},
-		"CanSeek":       {Value: true, Writable: false, Emit: prop.EmitTrue},
-		"CanControl":    {Value: true, Writable: false, Emit: prop.EmitFalse},
+		"PlaybackStatus": {Value: playbackStopped, Writable: false, Emit: prop.EmitTrue},
+		"LoopStatus":     {Value: loopNone, Writable: true, Emit: prop.EmitTrue},
+		"Rate":           {Value: float64(1), Writable: true, Emit: prop.EmitTrue},
+		"Shuffle":        {Value: false, Writable: true, Emit: prop.EmitTrue},
+		"Metadata":       {Value: playerMetadata{}, Writable: false, Emit: prop.EmitTrue},
+		"Volume":         {Value: float64(0), Writable: true, Emit: prop.EmitTrue},
+		"Position":       {Value: int64(0), Writable: false, Emit: prop.EmitFalse},
+		"MinimumRate":    {Value: playerMinimumRate, Writable: false, Emit: prop.EmitTrue},
+		"MaximumRate":    {Value: playerMaximumRate, Writable: false, Emit: prop.EmitTrue},
+		"CanGoNext":      {Value: true, Writable: false, Emit: prop.EmitTrue},
+		"CanGoPrevious":  {Value: true, Writable: false, Emit: prop.EmitTrue},
+		"CanPlay":        {Value: true, Writable: false, Emit: prop.EmitTrue},
+		"CanPause":       {Value: true, Writable: false, Emit: prop.EmitTrue},
+		"CanSeek":        {Value: true, Writable: false, Emit: prop.EmitTrue},
+		"CanControl":     {Value: true, Writable: false, Emit: prop.EmitFalse},
 	}
 
 	return p.propsSpec
